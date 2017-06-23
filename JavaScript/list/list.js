a774f85a8d3e73e17503e893ddf79e5e4e0871b3
Jfischer16 <Jfischer7722@gmail.com>
2017-06-22 21:36:19 -0400

    updated todo list and made some forEach examples

--- JavaScript/list/list.js
+++ JavaScript/list/list.js
@@ -6,13 +6,36 @@ var input = prompt("what would you like to do?");
 
 while (input !== "quit") {
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     } else if (input === "new") {
-
-        var newTodo = prompt("enter new todo");
-
-        todos.push(newTodo);
+        addTodo();
+    } else if (input === "delete") {
+        deleteTodo();
     }
+
     input = prompt("what would you like to do?");
 }
 console.log("ok, you quit the app");
+
+function listTodos() {
+    console.log("**********")
+    todos.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********")
+}
+
+function addTodo() {
+
+    var newTodo = prompt("enter new todo");
+
+    todos.push(newTodo);
+    console.log("added todo");
+}
+
+function deleteTodo() {
+    var index = prompt("enter index of todo to delete");
+
+    todos.splice(index, 1);
+    console.log("deleted todo");
+}
